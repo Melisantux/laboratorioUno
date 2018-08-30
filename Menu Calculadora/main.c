3102bd0b28728063590da550481d97417877ccab
Melisantux <meli.santuccio@gmail.com>
2018-08-30 16:12:23 -0300

    Calculadora c/4 operaciones funcionales

--- Menu Calculadora/main.c
+++ Menu Calculadora/main.c
@@ -2,31 +2,54 @@
 #include <stdlib.h>
 
 int seleccionDeOpcion(void);
-int ingresoPrimerOperando(void);
-int ingresoSegundoOperando(void);
+float ingresoPrimerOperando(void);
+float ingresoSegundoOperando(void);
 int seleccionOperacion(void);
-int operacionSuma(int primerOperando, int segundoOperando);
-int operacionResta(int primerOperando, int segundoOperando);
-float operacionDivision(int primerOperando, int segundoOperando);
+float operacionSuma(float primerOperando, float segundoOperando);
+float operacionResta(float primerOperando, float segundoOperando);
+float operacionDivision(float primerOperando, float segundoOperando);
+float operacionMultiplicacion (float primerOperando, float segundoOperando);
 
 int main()
 {
     int opcion;
     int opcion3;
-    int primerOperando;
-    int segundoOperando;
-    int suma;
-    int resta;
-    //float multiplicacion;
+    float primerOperando;
+    float segundoOperando;
+    float suma;
+    float resta;
+    int banderaPrimerOperando=0;
+    int banderaSegundoOperando=0;
+    int banderaSuma=0;
+    int banderaResta=0;
+    int banderaMultiplicacion=0;
+    int banderaDivision=0;
+    //int banderaFactorial=0;
+    float multiplicacion;
     float division;
-    //float factorial;
+    //int factorial;
 
     do
     {
         printf("\n_____________________________________________\n");
 
-        printf("\n1)Ingresar el primer operando. %d\n", primerOperando);
-        printf("2)Ingresar el segundo operando. %d\n", segundoOperando);
+        if (banderaPrimerOperando == 0)
+        {
+            printf("\n1)Ingresar el primer operando.\n");
+        }
+        else
+        {
+            printf("\n1)Ingresar el primer operando: %0.3f\n", primerOperando);
+        }
+
+        if (banderaSegundoOperando == 0)
+        {
+            printf("2)Ingresar el segundo operando.\n");
+        }
+        else
+        {
+            printf("2)Ingresar el segundo operando: %0.3f\n", segundoOperando);
+        }
         printf("3)Realizar operaciones.\n");
         printf("4)Informar resultados.\n");
         printf("5)Salir.\n");
@@ -39,20 +62,22 @@ int main()
         {
         case 1:
             primerOperando=ingresoPrimerOperando();
+            banderaPrimerOperando=1;
             system("cls");
             break;
         case 2:
             segundoOperando=ingresoSegundoOperando();
+            banderaSegundoOperando=1;
             system("cls");
             break;
         case 3:
-            printf("\n_____________________________________________\n");
+            printf("_____________________________________________\n");
 
             printf("\n1)Calcular la suma.\n");
             printf("2)Calcular la resta.\n");
             printf("3)Calcular la division.\n");
             printf("4)Calcular la multiplicacion.\n");
-            printf("5)Calcular el factorial.\n");
+            printf("5)Calcular el factorial.");
 
             printf("\n_____________________________________________\n");
 
@@ -62,28 +87,65 @@ int main()
             {
             case 1:
                 suma=operacionSuma(primerOperando, segundoOperando);
+                banderaSuma=1;
                 break;
             case 2:
                 resta=operacionResta(primerOperando, segundoOperando);
+                banderaResta=1;
                 break;
             case 3:
                 division=operacionDivision(primerOperando, segundoOperando);
-
+                banderaDivision=1;
+                break;
+            case 4:
+                multiplicacion=operacionMultiplicacion(primerOperando, segundoOperando);
+                banderaMultiplicacion=1;
             }
+
             system("cls");
             break;
         case 4:
-            printf("\n_____________________________________________\n");
-            printf("\nEl resultado de %d", primerOperando);
-            printf(" + %d", segundoOperando);
-            printf(" es: %d\n", suma);
-            printf("\n_____________________________________________\n");
-            printf("\nEl resultado de %d", primerOperando);
-            printf(" - %d", segundoOperando);
-            printf(" es: %d\n", resta);
-            printf("\nEl resultado de %d", primerOperando);
-            printf("/%d", segundoOperando);
-            printf(" es: %0.3f\n", division);
+            if (banderaSuma == 1)
+            {
+                printf("\n_____________________________________________\n");
+                printf("\nEl resultado de %0.3f", primerOperando);
+                printf(" + %0.3f", segundoOperando);
+                printf(" es: %0.3f", suma);
+                printf("\n_____________________________________________\n");
+            }
+            if (banderaResta == 1)
+            {
+                printf("\n_____________________________________________\n");
+                printf("\nEl resultado de %0.3f", primerOperando);
+                printf(" - %f", segundoOperando);
+                printf(" es: %0.3f", resta);
+                printf("\n_____________________________________________\n");
+
+            }
+            if (banderaDivision == 1 && segundoOperando !=0)
+            {
+                printf("\n_____________________________________________\n");
+                printf("\nEl resultado de %0.3f", primerOperando);
+                printf("/%0.3f", segundoOperando);
+                printf(" es: %0.3f", division);
+                printf("\n_____________________________________________\n");
+            } else
+            {
+                if (segundoOperando == 0)
+                {
+                    printf("\n_____________________________________________\n");
+                    printf("\nNo es posible dividir por cero.");
+                    printf("\n_____________________________________________\n");
+                }
+            }
+            if (banderaMultiplicacion == 1)
+            {
+                printf("\n_____________________________________________\n");
+                printf("\nEl resultado de %0.3f", primerOperando);
+                printf("* %0.3f", segundoOperando);
+                printf(" es: %0.3f", multiplicacion);
+                printf("\n_____________________________________________\n");
+            }
         }
     }
     while (opcion >= 1 && opcion <5);
@@ -102,20 +164,20 @@ int seleccionDeOpcion(void)
     return opcion;
 }
 
-int ingresoPrimerOperando()
+float ingresoPrimerOperando()
 {
-    int primerOperando;
+    float primerOperando;
     printf("Ingrese el primer operando: ");
-    scanf("%d", &primerOperando);
+    scanf("%f", &primerOperando);
 
     return primerOperando;
 }
 
-int ingresoSegundoOperando()
+float ingresoSegundoOperando()
 {
-    int segundoOperando;
+    float segundoOperando;
     printf("Ingrese el segundo operando: ");
-    scanf("%d", &segundoOperando);
+    scanf("%f", &segundoOperando);
 
     return segundoOperando;
 }
@@ -130,29 +192,37 @@ int seleccionOperacion(void)
     return opcion;
 }
 
-int operacionSuma(int primerOperando, int segundoOperando)
+float operacionSuma(float primerOperando, float segundoOperando)
 {
-    int suma;
-
+    float suma;
     suma= primerOperando + segundoOperando;
 
     return suma;
 }
 
-int operacionResta(int primerOperando, int segundoOperando)
+float operacionResta(float primerOperando, float segundoOperando)
 {
-    int resta;
-    system("cls");
+    float resta;
     resta= primerOperando - segundoOperando;
 
     return resta;
 }
 
-float operacionDivision(int primerOperando, int segundoOperando)
+float operacionDivision(float primerOperando, float segundoOperando)
 {
     float division;
-
-    division=(float)primerOperando/segundoOperando;
+    if (segundoOperando != 0)
+    {
+        division=primerOperando/segundoOperando;
+    }
 
     return division;
+}
+
+float operacionMultiplicacion(float primerOperando, float segundoOperando)
+{
+    float multiplicacion;
+    multiplicacion= primerOperando * segundoOperando;
+
+    return multiplicacion;
 }
