07962d6c7370795579a9180b982fa2ae7ab994a0
Melanie <meli.santuccio@gmail.com>
2018-09-11 12:16:32 -0300

    Clase 11-09

--- Vectores Paralelos/main.c
+++ Vectores Paralelos/main.c
@@ -2,15 +2,35 @@
 #include <stdlib.h>
 #define T 3
 
+void cargarAlumnos(int[], char[][21], int[], float[], int);
+//crear funcion mostrarAlumnos.
+//Rehacer de cero.
+
 int main()
 {
     int legajos[T];
     char nombres[T][21];
     int notas[T];
     float altura[T];
+    char mail[T][50];
     int i;
 
+    cargarAlumnos(legajos, nombres, notas, altura, T);
+
+    printf("%4s %20s %2s %5s\n", "Legajo", "Nombre", "Nota", "Altura");
+
     for(i=0; i<T; i++)
+    {
+        printf("%4d  %20s  %2d  %5.2f\n", legajos[i], nombres[i], notas[i], altura[i]);
+    }
+
+    return 0;
+}
+
+void cargarAlumnos(int legajos[], char nombres[][21], int notas[], float altura[], int tamañoVector)
+{
+    int i;
+    for(i=0; i<tamañoVector; i++)
     {
         printf("Ingrese legajo: ");
         scanf("%d", &legajos[i]);
@@ -22,14 +42,4 @@ int main()
         printf("Ingrese altura: ");
         scanf("%f", &altura[i]);
     }
-    printf("%4s %20s %2s %5s\n", "Legajo", "Nombre", "Nota", "Altura");
-
-    for(i=0; i<T; i++)
-    {
-        printf("%4d  %20s  %2d  %5.2f\n", legajos[i], nombres[i], notas[i], altura[i]);
-    }
-
-
-
-    return 0;
 }
